011ffe65fb81d3bb53032a04814646f7fc464b3e
eskoglund <eric.v.skoglund@gmail.com>
2019-11-18 18:16:29 +0100

    login is working

--- view/Login.js
+++ view/Login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Alert, ImageBackground, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, TextInput, View, Button, Alert, ImageBackground, TouchableOpacity} from 'react-native';
 
 export default class Login extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -11,7 +11,9 @@ export default class Login extends Component {
     super(props);
     this._isMounted = false;
     this.state = {
-      db: require("../dbIp.json")
+      db: require("../dbIp.json"),
+      id: "",
+      pw: ""
     }
   }
 
@@ -23,16 +25,48 @@ export default class Login extends Component {
     this._isMounted = false;
   }
 
+  identifyDevice() {
+    fetch("http://" + this.state.db.ip + "/_db/HomeAssist/CRUD_b/login/" + this.state.id, {
+      method: "GET",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.errorNum === "404") {
+        alert("No connection to the device.");
+    } else if (this.state.pw === data.password) {
+        this.props.navigation.navigate("WelcomeScreen");
+    } else {
+        alert("Wrong id or password.");
+      }
+    })
+  }
+
   render() {
     return (
       <ImageBackground source={require("../images/loginBackground.jpg")} style={styles.container}>
-        <Text style={styles.text}>Home Assist</Text>
+        <Text style={styles.text}>ELIAS</Text>
           <View style={styles.button}>
+            <TextInput style={styles.inputBox}
+                onChangeText={(id) => this.setState({ id })}
+                underlineColorAndroid='rgba(0,0,0,0)'
+                placeholder="Enter device id"
+                placeholderTextColor = "#002f6c"
+                selectionColor="#fff"/>
+            <TextInput style={styles.inputBox}
+                onChangeText={(pw) => this.setState({ pw })}
+                underlineColorAndroid='rgba(0,0,0,0)'
+                placeholder="Enter password"
+                secureTextEntry={true}
+                placeholderTextColor = "#002f6c"/>
             <Button
               title="Connect to your home"
               color="green"
               style={styles.button}
-              onPress={() => this.props.navigation.navigate("WelcomeScreen")}>
+              onPress={() => this.identifyDevice()}>
             </Button>
           </View>
       </ImageBackground>
@@ -41,24 +75,34 @@ export default class Login extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    opacity: 50,
-  },
-  text: {
-    fontSize: 50,
-    color: "white",
-    fontStyle: "italic",
-    fontWeight: "500",
-    textDecorationLine: "underline",
-    textShadowColor: "green",
-    textShadowRadius: 8,
-    bottom: 150
-  },
-  button: {
-    position: "absolute",
-    bottom: 160
-  }
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 50,
+    },
+    text: {
+        fontSize: 70,
+        color: "white",
+        fontStyle: "italic",
+        fontWeight: "500",
+        textDecorationLine: "underline",
+        textShadowColor: "green",
+        textShadowRadius: 8,
+        bottom: 150
+    },
+    button: {
+        position: "absolute",
+        bottom: 300
+    },
+    inputBox: {
+        width: 300,
+        backgroundColor: '#eeeeee',
+        borderRadius: 25,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#002f6c',
+        marginVertical: 10,
+        bottom: 50
+    }
 });
